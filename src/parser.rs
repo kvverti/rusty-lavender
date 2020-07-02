b01b2575fc244f491187e7ef85add273ea8a5e4c
Thalia Nero <chrisvn00@gmail.com>
2020-07-01 23:42:37 -0400

    Move tokenizer to child module.

--- src/parser.rs
+++ src/parser.rs
@@ -2,14 +2,8 @@ use std::ops::RangeTo;
 
 use nom::{IResult, Offset, Slice};
 
-/// Parsers for names and operators.
-mod identifier;
-/// Parsers for separators and keywords.
-mod fixed;
-/// Token delimiters: whitespace, comments, and new lines.
-mod delimiter;
-/// Literals: bool, int, float.
-mod literal;
+/// The tokenizer.
+mod token;
 
 type Source<'a> = &'a str;
 
---
+++ src/parser/token.rs
@@ -0,0 +1,8 @@
+/// Parsers for names and operators.
+mod identifier;
+/// Parsers for separators and keywords.
+mod fixed;
+/// Token delimiters: whitespace, comments, and new lines.
+mod delimiter;
+/// Literals: bool, int, float.
+mod literal;
