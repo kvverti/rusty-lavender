aa4a1acc44eccbebb8025bdafa0529a33e0641b4
Thalia Nero <chrisvn00@gmail.com>
2020-07-27 02:50:38 -0400

    Add opcode declaration region comments.

--- src/code.rs
+++ src/code.rs
@@ -5,6 +5,8 @@ use crate::value::LvValue;
 #[allow(dead_code)]
 #[derive(Copy, Clone)]
 pub enum Opcode {
+    // immediate opcodes
+
     /// Load the unit value.
     UnitValue,
     /// Load the value `True`.
@@ -15,24 +17,39 @@ pub enum Opcode {
     Value(SymbolicReference),
     /// Load an integer value in a single-byte range. Useful not to overload the symbol table.
     IntValue(i8),
+
+    // value marshalling opcodes
+
     /// A move of the given argument (or function local) to the stack.
     MoveArg(u8),
     /// A copy of the given argument (or function local) to the stack.
     CopyArg(u8),
     /// A move from the stack to the given argument (or function local).
     MoveArgTo(u8),
+
+    // function application opcodes
+
     /// Function application.
     Apply,
     /// Fully evaluate the top expression. Used sparingly.
     Eval,
     /// A return from a function to the caller.
     Return,
+
+    // value destructuring opcodes
+
     /// Destructures a vect or tuple.
     MatchTuple,
+
+    // jump opcodes
+
     /// Relative jump to the given offset.
     Jump(i32),
     /// Branch if false.
     BranchFalse(i32),
+
+    // intrinsic opcodes
+
     /// An "escape hatch" into the native environment. Used to implement intrinsic operations
     /// such as integer addition. This opcode shall never appear in serialized forms.
     Intrinsic(u8, fn(&mut [LvValue]) -> LvValue),
