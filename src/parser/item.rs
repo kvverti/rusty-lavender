6d905d23377d300fce1db4ba3af4a1b0c9e1a600
Thalia Nero <chrisvn00@gmail.com>
2020-07-29 02:26:35 -0400

    Add optional type annotation to the def parser.

--- src/parser/item.rs
+++ src/parser/item.rs
@@ -1,8 +1,8 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::combinator::map;
+use nom::combinator::{map, opt};
 use nom::multi::{count, many1};
-use nom::sequence::{pair, preceded, tuple};
+use nom::sequence::{delimited, pair, preceded, tuple};
 
 use crate::parser::fixity::prefix_operator;
 use crate::parser::ParseResult;
@@ -11,6 +11,7 @@ use crate::parser::primary::Primary;
 use crate::parser::token::{TokenStream, TokenValue};
 use crate::parser::token::fixed::{Keyword, Separator};
 use crate::parser::token::identifier::Identifier;
+use crate::parser::typedecl::TypeExpression;
 use crate::parser::value::ValueExpression;
 
 /// A function definition `def f a => b`.
@@ -18,6 +19,8 @@ use crate::parser::value::ValueExpression;
 pub struct FunctionDefinition {
     /// The name of the function.
     pub name: Identifier,
+    /// The declared type of the function (optional).
+    pub typ: Option<TypeExpression>,
     /// The initial parameter patterns (may be empty).
     pub params: Vec<PatternPrimary>,
     /// The one or more function bodies for this function.
@@ -29,14 +32,20 @@ impl FunctionDefinition {
         map(
             tuple((
                 preceded(tag(TokenValue::from(Keyword::Def)), prefix_operator),
+                opt(delimited(
+                    tag(TokenValue::from(Separator::Colon)),
+                    TypeExpression::parse,
+                    tag(TokenValue::from(Separator::Semicolon)),
+                )),
                 many1(PatternPrimary::parse),
                 alt((
-                    count(FunctionBody::single, 1),
                     many1(FunctionBody::multiple),
+                    count(FunctionBody::single, 1),
                 ))
             )),
-            |(name, params, bodies)| Self {
+            |(name, typ, params, bodies)| Self {
                 name,
+                typ,
                 params,
                 bodies,
             },
@@ -81,6 +90,7 @@ mod tests {
     use crate::parser::scoped::ScopedIdentifier;
     use crate::parser::token::identifier::{Name, Operator};
     use crate::parser::token::Token;
+    use crate::parser::typedecl::TypePrimary;
     use crate::parser::value::ValuePrimary;
 
     use super::*;
@@ -89,6 +99,7 @@ mod tests {
     fn single() {
         let expected = FunctionDefinition {
             name: Identifier::Operator(Operator("@".to_owned())),
+            typ: None,
             params: vec![
                 PatternPrimary::Identifier(ScopedIdentifier::from(Identifier::Name(Name("a".to_owned())))),
                 PatternPrimary::SubPattern(Box::new(Pattern::Application(PrefixApply {
@@ -123,6 +134,7 @@ mod tests {
     fn multiple() {
         let expected = FunctionDefinition {
             name: Identifier::Name(Name("bind".to_owned())),
+            typ: None,
             params: vec![PatternPrimary::Identifier(ScopedIdentifier::from(Identifier::Name(Name("f".to_owned()))))],
             bodies: vec![
                 FunctionBody {
@@ -147,6 +159,48 @@ mod tests {
             def bind f
                 ; (Some a) => f a
                 ; _ => None
+        ";
+        let (_, result) = Token::parse_sequence(input).expect("Unable to parse tokens");
+        let result = FunctionDefinition::parse(TokenStream(result.as_slice()));
+        assert!(result.is_ok(), "Expected ok result, got {:?}", result);
+        let (rest, result) = result.unwrap();
+        assert_eq!(rest.0, &[]);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn with_type() {
+        let expected = FunctionDefinition {
+            name: Identifier::Name(Name("const".to_owned())),
+            typ: Some(TypeExpression::InfixTypeApplication(InfixApply {
+                func: Identifier::Operator(Operator("->".to_owned())),
+                args: vec![
+                    InfixPrimary::Primary(TypePrimary::TypeVariable(Name("a".to_owned()))),
+                    InfixPrimary::Primary(TypePrimary::TypeSubExpression(Box::new(
+                        TypeExpression::InfixTypeApplication(InfixApply {
+                            func: Identifier::Operator(Operator("->".to_owned())),
+                            args: vec![
+                                InfixPrimary::Primary(TypePrimary::TypeVariable(Name("b".to_owned()))),
+                                InfixPrimary::Primary(TypePrimary::TypeVariable(Name("a".to_owned()))),
+                            ],
+                        })
+                    )))
+                ],
+            })),
+            params: vec![
+                PatternPrimary::Identifier(ScopedIdentifier::from(Identifier::Name(Name("a".to_owned())))),
+                PatternPrimary::Blank,
+            ],
+            bodies: vec![
+                FunctionBody {
+                    params: vec![],
+                    body: ValueExpression::Primary(ValuePrimary::Identifier(ScopedIdentifier::from(Identifier::Name(Name("a".to_owned()))))),
+                }
+            ],
+        };
+        let input = "
+            def const: 'a -> ('b -> 'a);
+                a _ => a
         ";
         let (_, result) = Token::parse_sequence(input).expect("Unable to parse tokens");
         let result = FunctionDefinition::parse(TokenStream(result.as_slice()));
